d5e5659a9239434f17eceec3ae93045e9b708ac6
rusolm <leandromarquesp88@gmail.com>
2018-11-07 09:18:18 +0100

    feat(station.ts): crate station model

---
+++ my-app/src/app/station.ts
@@ -0,0 +1,13 @@
+export class Station {
+  id: number;
+  type: string;
+  latitude: number;
+  longitude: number;
+  streetName: string;
+  streetNumber: number;
+  altitude: number;
+  slots: number;
+  bikes: number;
+  nearbyStations: string;
+  status: string;
+}
