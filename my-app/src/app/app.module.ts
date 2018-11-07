97dbff7f1c94674af64a2101c054a4f3eb03b6f4
rusolm <leandromarquesp88@gmail.com>
2018-11-07 16:02:02 +0100

    feat(material UI): add angular material UI

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,17 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
 import { AgmCoreModule } from '@agm/core';
 import { environment } from 'src/environments/environment';
+import { StationComponent } from './station/station.component';
 
 @NgModule({
-  declarations: [AppComponent, MapComponent],
+  declarations: [AppComponent, MapComponent, StationComponent],
   imports: [
   BrowserModule,
   HttpClientModule,
+  MatCardModule,
+  MatIconModule,
   AgmCoreModule.forRoot({
     apiKey: environment.GMAPS_API_KEY
     })
