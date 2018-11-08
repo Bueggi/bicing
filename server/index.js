df05c58d84be0faf477948cf1ac7096104f3ff60
rusolm <leandromarquesp88@gmail.com>
2018-11-08 16:34:20 +0100

    refactor(server): add temporary dev tests

--- server/index.js
+++ server/index.js
@@ -7,10 +7,17 @@ const app = new Koa();
 const router = new Router();
 
 router.get('/stations', async (ctx, next) => {
-  ctx.body = await rp('http://wservice.viabicing.cat/v2/stations');
+  console.log('new request to bicing');
+
+  const info = await rp('http://wservice.viabicing.cat/v2/stations');
+  console.log('55: solts:', JSON.parse(info).stations[52].slots);
+
+  ctx.body = info;
 });
 
 app.use(cors());
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log('server running on port 3000');
+});
