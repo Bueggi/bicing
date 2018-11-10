71f12aa13f46781cb1701eb58ed56e2d5b73ad0e
rusolm <leandromarquesp88@gmail.com>
2018-11-10 10:24:51 +0100

    feat(map.component): add geolocation to center map in current geolocalization

--- my-app/src/app/map/map.component.ts
+++ my-app/src/app/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { Component, Input, EventEmitter, Output, OnInit } from '@angular/core';
 import { Station } from '../station';
 
 @Component({
@@ -6,12 +6,13 @@ import { Station } from '../station';
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
   })
-export class MapComponent {
+export class MapComponent implements OnInit {
   title: string = 'Select destiny station';
   // map initial properties: center & zoom
-  lat: number = 41.382894;
-  lng: number = 2.177432;
-  zoom: number = 15;
+  currentLat: number;
+  currentLong: number;
+  zoom: number = 14.5;
+
   image: object = {
     url: '../../assets/2019_TImberjack_NX_Eagle_27.5_Org-uc-1_.jpg',
     scaledSize: {
@@ -30,7 +31,20 @@ export class MapComponent {
 
   constructor () {}
 
+  ngOnInit () {
+    this.getUserLocation();
+  }
+
   clickedMarker (station) {
     this.clickedStation.emit(station);
+  }
+
+  getUserLocation () {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        this.currentLat = position.coords.latitude;
+        this.currentLong = position.coords.longitude;
+      });
+    }
   }
 }
