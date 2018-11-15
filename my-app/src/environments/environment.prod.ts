4d22e0e9293a616eed5bb4509aeaa5afbd1fb3a9
Christopher Buecklein <christopher_b@gmx.de>
2018-11-15 16:44:13 +0100

    Remove environment.

--- my-app/src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-<<<<<<< HEAD
-  GMAPS_API_KEY: 'AIzaSyA2f_YYdMwSnBAVLUTD8ymt0CWIY-bl52c'
-=======
-  GMAPS_API_KEY: ''
->>>>>>> 05fc3198af37763ec1c5c2991f35277119015538
-};
