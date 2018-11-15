34a244b5bf3d680f091cf08723116e66dd1a7fa9
Christopher Buecklein <christopher_b@gmx.de>
2018-11-15 16:38:58 +0100

    changed: edited .gitignore and env

--- my-app/src/environments/environment.prod.ts
+++ my-app/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
 <<<<<<< HEAD
-  GMAPS_API_KEY: 'AIzaSyBz0TrXSdyDc0tyUPG3eumGPW8lr-TB9eE'
+  GMAPS_API_KEY: 'AIzaSyA2f_YYdMwSnBAVLUTD8ymt0CWIY-bl52c'
 =======
   GMAPS_API_KEY: ''
 >>>>>>> 05fc3198af37763ec1c5c2991f35277119015538
