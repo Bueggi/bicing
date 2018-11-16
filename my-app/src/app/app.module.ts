62394e08d58c997fe4e879dfb3df64ff213bc0a9
Amy Kirasack <amykirasack@gmail.com>
2018-11-16 10:47:21 +0100

    build: Update appmodulets imports

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -2,23 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
-import { MapComponent } from './map/map.component';
 import { AgmCoreModule, GoogleMapsAPIWrapper } from '@agm/core';
 import { AgmDirectionModule } from 'agm-direction';
 import { environment } from '../environments/environment.prod';
-import { StationComponent } from './station/station.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { MatComponentsModule } from './mat-components.module';
-import { NoSlotsDialogComponent } from './no-slots-dialog/no-slots-dialog.component';
-import { HeaderComponent } from './header/header.component';
-import { LessThanMinComponent } from './less-than-min/less-than-min.component';
 import { AppRoutingModule } from './app-routing.module';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { FavoriteStationsComponent } from './favorite-stations/favorite-stations.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
-import { NavbarComponent } from './navbar/navbar.component';
-import { NearestStationsComponent } from './nearest-stations/nearest-stations.component';
+import { MatComponentsModule } from './mat-components.module';
+import { AppComponent } from './app.component';
+
+import { MapComponent } from './components/map/map.component';
+import { FavoriteStationsComponent } from './components//favorite-stations/favorite-stations.component';
+import { NearestStationsComponent } from './components/nearest-stations/nearest-stations.component';
+import { StationComponent } from './components/station/station.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HeaderComponent } from './components/header/header.component';
+
+import { NoSlotsDialogComponent } from './view-components/no-slots-dialog/no-slots-dialog.component';
+import { LessThanMinComponent } from './view-components/less-than-min/less-than-min.component';
+import { PageNotFoundComponent } from './view-components/page-not-found/page-not-found.component';
+import { NavbarComponent } from './view-components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
